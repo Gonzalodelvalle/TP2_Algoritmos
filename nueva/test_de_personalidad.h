9c54a04558f283baac87fd0513ccdafba287c243
Gonzalo <dgonzalo@fi.uba.ar>
2021-07-25 12:30:19 -0300

    Juego entregado (Ver revision reentrega y mejorar)

---
+++ nueva/test_de_personalidad.h
@@ -0,0 +1,14 @@
+#ifndef __TEST_DE_PERSONALIDAD_H__
+#define __TEST_DE_PERSONALIDAD_H__
+
+#define POLAR 'I'
+#define PANDA 'P'
+#define PARDO 'G'
+
+/*
+.Pre: -
+.Post: Asigna el valor correspondiente a la personalidad del jugador, segun su puntaje
+*/
+void test_de_personalidad(char *personalidad_detectada);
+
+#endif //__TEST_DE_PERSONALIDAD_H__
